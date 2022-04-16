c372f75f852501cfcdf881c12efd5ce23b5f200a
uday-g-ithub15 <mudayhasan@gmail.com>
2022-04-17 02:37:47 +0600

    title,active-route,some-style

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
+    const activeLinkStyle = ({isActive}) => {
+        return {
+            backgroundColor: isActive ? 'tomato' : '',
+            color:isActive ? 'white' : ''
+        }
+    }
     return (
         <nav>
             <div className="photography-brand">
                 <h3><span>Candid</span> Shots</h3>
             </div>
             <div className="nav-items">
-                <Link to={'/'}>Home</Link> 
-                <Link to={'/Services'}>Services</Link> 
-                <Link to={'/about'}>About</Link> 
-                <Link to={'/login'}>Login</Link> 
-                <Link to={'/register'}>Register </Link> 
+                <NavLink   style={activeLinkStyle} to={'/'}>Home</NavLink> 
+                <NavLink   style={activeLinkStyle} to={'/Services'}>Services</NavLink> 
+                <NavLink   style={activeLinkStyle} to={'/about'}>About</NavLink> 
+                <NavLink   style={activeLinkStyle} to={'/login'}>Login</NavLink> 
+                <NavLink   style={activeLinkStyle} to={'/register'}>Register </NavLink> 
             </div>
         </nav>
     );
